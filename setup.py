d8d4b1ca3b08d053f9f6597248d562e0e813fe05
Yan <yans@yancomm.net>
2024-11-14 16:04:56 -0700

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ezmp',
-    version='3.3.1.3.3.7',
+    version='3.3.3.1.3.3.7',
     py_modules=['ezmp'],
     description='Multiprocessing done RIGHT.',
     url='https://github.com/zardus/ezmp',
